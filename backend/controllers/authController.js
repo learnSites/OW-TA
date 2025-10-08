3f654c74a228ab0ccbd1da7d55e6f61049a03128
learnSites <learnthingsbetterthenever@gmail.com>
2025-10-08 16:43:55 +0530

    removed serect

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,12 +1,11 @@
 const googleAuth = (req, res) => {
-  const clientId = '936609673781-3474auo6bn3gka6shfr4s7j8km09qs2m.apps.googleusercontent.com';
-  const redirectUri = 'http://localhost:5000/auth/google/callback';
 
-  const url = `https://accounts.google.com/o/oauth2/v2/auth?` +
-              `client_id=${clientId}` +
-              `&redirect_uri=${redirectUri}` +
-              `&response_type=code` +
-              `&scope=openid%20email%20profile`;
+
+//   const url = `https://accounts.google.com/o/oauth2/v2/auth?` +
+//               `client_id=${clientId}` +
+//               `&redirect_uri=${redirectUri}` +
+//               `&response_type=code` +
+//               `&scope=openid%20email%20profile`;
 
   res.redirect(url);    
 };
